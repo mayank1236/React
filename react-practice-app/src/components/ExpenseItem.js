75cd4069aa83f1cc73c2e8399b948379d951d4b9
Mayank <mayank@webenertia.com>
2022-09-15 01:03:46 +0530

    used props and dynamic data

--- react-practice-app/src/components/ExpenseItem.js
+++ react-practice-app/src/components/ExpenseItem.js
@@ -1,17 +1,13 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
 
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle =  'Car Insurance';
-    const expenseAmount = 294.67;
-    
     return (
         <div className="expense-item">
-            <div>15-September-2022</div>
+            <div>{props.date}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$294.56</div>
+                <h2>{props.title}</h2> 
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
