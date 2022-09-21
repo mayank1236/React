71df7ca26cf095ae3c9c23ddec229bc053e8795a
Mayank <mayank@webenertia.com>
2022-09-21 19:06:44 +0530

    multiple useState, event handling, form input

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
 
 const App = () => {
   const expenses =  [
@@ -26,6 +28,7 @@ const App = () => {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <NewExpense></NewExpense>
       <Expenses items={expenses}></Expenses>
     </div>
   );
